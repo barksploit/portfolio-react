6599b5c27c8cd9ae13171429847b28a0d59809fd
Thomas Griffin <tom.a.griffin@outlook.com>
2022-07-09 10:34:24 +0100

    Create skeleton desktop navigation

--- src/components/header-navigation.js
+++ src/components/header-navigation.js
@@ -3,7 +3,18 @@ import '../styles/header-navigation.scss';
 export default function Header() {
     return (
 
-        <nav>
+        <nav className="desktop-navigation">
+
+            <ul>
+
+                <li><a href="#">intro.js</a></li>
+                <li><a href="#">about.js</a></li>
+                <li><a href="#">experience.js</a></li>
+                <li><a href="#">work.js</a></li>
+                <li><a href="#">end.js</a></li>
+
+
+            </ul>
 
         </nav>
 
