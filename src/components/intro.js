ef78b35e37e38fb68821920d91c0c8fbd0f704b3
Thomas Griffin <tom.a.griffin@outlook.com>
2022-07-09 12:02:03 +0100

    Add subtitle and description to intro component

--- src/components/intro.js
+++ src/components/intro.js
@@ -5,7 +5,11 @@ import 'react-typist/dist/Typist.css';
 export default function Intro() {
     return (
         <section id="intro">
-            <h1><Typist>Hello, Barksploit<Typist.Backspace count={10} delay={200} /> Thomas here. </Typist></h1>
+            <div class="intro-content-wrapper">
+                <h1 className="intro-header"><Typist avgTypingDelay={100}>Hello, Barksploit<Typist.Backspace count={10} delay={200} /> Thomas here. </Typist></h1>
+                <h2 className="intro-subtitle">Full-Stack Web Developer</h2>
+                <p className="intro-description">Nullam ut imperdiet dui. Donec tempus sem vestibulum est volutpat vestibulum. Quisque tempor enim a urna malesuada tempor. Sed blandit aliquet metus, ac efficitur felis efficitur quis. Sed fermentum tempus pulvinar.</p>
+            </div>
         </section>
     );
 }
