f61f2cb660129de2ad9f0196d03ad66c9ed94073
Thomas Griffin <tom.a.griffin@outlook.com>
2022-07-09 11:29:50 +0100

    Style intro and header navigation components

---
+++ src/components/header-branding.js
@@ -0,0 +1,5 @@
+import '../styles/header-branding.scss';
+
+export default function Branding() {
+    return <div className="brand-icon-wrapper"></div>;
+}
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
 import '../styles/header.scss';
 import HeaderNav from './header-navigation';
+import Branding from './header-branding';
 
 export default function Header() {
     return (
 
         <header>
+
+            <Branding />
 
             <HeaderNav />
 
