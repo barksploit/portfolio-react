9c830c03fa4b946389ed1ada93c0d0b110310813
Thomas Griffin <tom.a.griffin@outlook.com>
2022-07-10 21:15:27 +0100

    MUI social media links and call-to-action buttons

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,15 +1,54 @@
 import '../styles/intro.scss';
 import Typist from 'react-typist';
 import 'react-typist/dist/Typist.css';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            // Purple and green play nicely together.
+            main: '#FFF',
+        },
+        secondary: {
+            // This is green.A700 as hex.
+            main: '#11cb5f',
+        },
+    },
+});
 
 export default function Intro() {
     return (
         <section id="intro">
             <div class="intro-content-wrapper">
-                <h1 className="intro-header"><Typist avgTypingDelay={100}>Hello, Barksploit<Typist.Backspace count={10} delay={200} /> Thomas here. </Typist></h1>
+                <h1 className="intro-header"><Typist avgTypingDelay={100}>Hello, BarkSploit<Typist.Backspace count={10} delay={200} /> Thomas here. </Typist></h1>
                 <h2 className="intro-subtitle">Full-Stack Web Developer</h2>
-                <p className="intro-description">Nullam ut imperdiet dui. Donec tempus sem vestibulum est volutpat vestibulum. Quisque tempor enim a urna malesuada tempor. Sed blandit aliquet metus, ac efficitur felis efficitur quis. Sed fermentum tempus pulvinar.</p>
-            </div>
-        </section>
+                <div className="intro-social-media-links">
+                    <Link href="https://twitter.com/barksploit" className="intro-social-media-link" target="_blank" rel="noopener">
+                        <TwitterIcon />
+                    </Link>
+                    <Link href="https://github.com/" className="intro-social-media-link" target="_blank" rel="noopener">
+                        <GitHubIcon />
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/thomas-anthony-griffin/" className="intro-social-media-link" target="_blank" rel="noopener">
+                        <LinkedInIcon />
+                    </Link>
+                    <Link href="https://www.instagram.com/barksploit/" className="intro-social-media-link" target="_blank" rel="noopener">
+                        <InstagramIcon />
+                    </Link>
+                </div >
+                <ThemeProvider theme={theme}>
+                    <ButtonGroup variant="outlined" aria-label="outlined primary button group">
+                        <Button>Contact me</Button>
+                        <Button>Learn more</Button>
+                    </ButtonGroup></ThemeProvider>
+            </div >
+        </section >
     );
 }
