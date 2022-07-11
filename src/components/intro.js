ac2d662bfdf7552b66b5fe07d7cd91505cf4bb43
Thomas Griffin <tom.a.griffin@outlook.com>
2022-07-11 23:14:53 +0100

    Install react-fade-in npm package and implement it to fade in Intro component

--- src/components/intro.js
+++ src/components/intro.js
@@ -7,6 +7,7 @@ import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import FadeIn from 'react-fade-in';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { gsap } from "gsap";
@@ -53,27 +54,29 @@ export default function Intro() {
         <div id="intro-wrapper">
             <section id="intro" ref={introRef}>
                 <div class="intro-content-wrapper">
-                    <h1 className="intro-header"><Typist avgTypingDelay={100}>Hello, BarkSploit<Typist.Backspace count={10} delay={200} /> Thomas here. </Typist></h1>
-                    <h2 className="intro-subtitle">Full-Stack Web Developer</h2>
-                    <div className="intro-social-media-links">
-                        <Link href="https://twitter.com/barksploit" className="intro-social-media-link" target="_blank" rel="noopener">
-                            <TwitterIcon />
-                        </Link>
-                        <Link href="https://github.com/barksploit" className="intro-social-media-link" target="_blank" rel="noopener">
-                            <GitHubIcon />
-                        </Link>
-                        <Link href="https://www.linkedin.com/in/thomas-anthony-griffin/" className="intro-social-media-link" target="_blank" rel="noopener">
-                            <LinkedInIcon />
-                        </Link>
-                        <Link href="https://www.instagram.com/barksploit/" className="intro-social-media-link" target="_blank" rel="noopener">
-                            <InstagramIcon />
-                        </Link>
-                    </div >
-                    <ThemeProvider theme={theme}>
-                        <ButtonGroup variant="outlined" aria-label="outlined primary button group">
-                            <Button>Contact me</Button>
-                            <Button onClick={() => { handleLearnMoreClick() }}>Learn more</Button>
-                        </ButtonGroup></ThemeProvider>
+                    <FadeIn delay={100}>
+                        <h1 className="intro-header"><Typist avgTypingDelay={100}>Hello, BarkSploit<Typist.Backspace count={10} delay={200} /> Thomas here. </Typist></h1>
+                        <h2 className="intro-subtitle">Full-Stack Web Developer</h2>
+                        <div className="intro-social-media-links">
+                            <Link href="https://twitter.com/barksploit" className="intro-social-media-link" target="_blank" rel="noopener">
+                                <TwitterIcon />
+                            </Link>
+                            <Link href="https://github.com/barksploit" className="intro-social-media-link" target="_blank" rel="noopener">
+                                <GitHubIcon />
+                            </Link>
+                            <Link href="https://www.linkedin.com/in/thomas-anthony-griffin/" className="intro-social-media-link" target="_blank" rel="noopener">
+                                <LinkedInIcon />
+                            </Link>
+                            <Link href="https://www.instagram.com/barksploit/" className="intro-social-media-link" target="_blank" rel="noopener">
+                                <InstagramIcon />
+                            </Link>
+                        </div >
+                        <ThemeProvider theme={theme}>
+                            <ButtonGroup variant="outlined" aria-label="outlined primary button group">
+                                <Button>Contact me</Button>
+                                <Button onClick={() => { handleLearnMoreClick() }}>Learn more</Button>
+                            </ButtonGroup></ThemeProvider>
+                    </FadeIn>
                 </div>
             </section>
         </div>
