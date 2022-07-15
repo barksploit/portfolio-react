1a46961082f9f093fc0b3c2dcd120ed6bc39224a
Thomas Griffin <tom.a.griffin@outlook.com>
2022-07-15 04:40:54 +0100

    Added content to about component

--- src/components/about.js
+++ src/components/about.js
@@ -2,6 +2,24 @@ import '../styles/about.scss';
 
 export default function About() {
     return (
-        <section id="about"><h2>About</h2></section>
+        <section id="about">
+            <div class="about-content-wrapper">
+                <h1>about me</h1>
+
+                <p>I am a UWE Bristol BSc Graduate based in the South West UK. I specialise in Full-Stack Web Development. I intend on going back to University and achieving a Masters degree in Cyber Security at UWE Bristol.</p>
+
+                <p>Here are some things I've worked with:</p>
+
+                <ul>
+                    <li>Javascript ES6+</li>
+                    <li>React.js</li>
+                    <li>PHP 8+</li>
+                    <li>Laravel</li>
+                    <li>HTML & CSS</li>
+                    <li>SCSS/SASS</li>
+                </ul>
+
+            </div>
+        </section>
     );
 }
