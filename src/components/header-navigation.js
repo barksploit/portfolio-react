c72cfa08f5ddc1b3c9de0d3880c11e912d7d6155
Thomas Griffin <tom.a.griffin@outlook.com>
2022-07-15 04:18:27 +0100

    Update desktop navigation component items

--- src/components/header-navigation.js
+++ src/components/header-navigation.js
@@ -9,11 +9,11 @@ export default function HeaderNav() {
 
             <ul>
 
-                <li><Link activeClass="active" to="intro-wrapper" spy={true} smooth={true} duration={500}>intro.js</Link></li>
-                <li><Link activeClass="active" to="about" spy={true} smooth={true} duration={500}>about.js</Link></li>
-                <li><Link activeClass="active" to="experience" spy={true} smooth={true} duration={500}>experience.js</Link></li>
-                <li><Link activeClass="active" to="work" spy={true} smooth={true} duration={500}>work.js</Link></li>
-                <li><Link activeClass="active" to="credits" spy={true} smooth={true} duration={500}>end.js</Link></li>
+                <li><Link activeClass="active" to="intro-wrapper" spy={true} smooth={true} duration={500}>intro()</Link></li>
+                <li><Link activeClass="active" to="about" spy={true} smooth={true} duration={500}>about()</Link></li>
+                <li><Link activeClass="active" to="experience" spy={true} smooth={true} duration={500}>experience()</Link></li>
+                <li><Link activeClass="active" to="work" spy={true} smooth={true} duration={500}>work()</Link></li>
+                <li><Link activeClass="active" to="credits" spy={true} smooth={true} duration={500}>credits()</Link></li>
 
 
             </ul>
