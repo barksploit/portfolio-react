e44d9e7308353eedd751a3ccc61d6ee9d3c348d8
Thomas Griffin <tom.a.griffin@outlook.com>
2022-07-15 13:51:07 +0100

    Add HTML to about title and add MUI chips

--- src/components/about.js
+++ src/components/about.js
@@ -1,24 +1,39 @@
 import '../styles/about.scss';
-
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 export default function About() {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                // Purple and green play nicely together.
+                main: '#FFF',
+            },
+            secondary: {
+                // This is green.A700 as hex.
+                main: '#11cb5f',
+            },
+        },
+    });
     return (
         <section id="about">
             <div class="about-content-wrapper">
-                <h1>about me</h1>
+                <h1>&lt;about me&gt;</h1>
 
                 <p>I am a UWE Bristol BSc Graduate based in the South West UK. I specialise in Full-Stack Web Development. I intend on going back to University and achieving a Masters degree in Cyber Security at UWE Bristol.</p>
 
                 <p>Here are some things I've worked with:</p>
-
-                <ul>
-                    <li>Javascript ES6+</li>
-                    <li>React.js</li>
-                    <li>PHP 8+</li>
-                    <li>Laravel</li>
-                    <li>HTML & CSS</li>
-                    <li>SCSS/SASS</li>
-                </ul>
-
+                <ThemeProvider theme={theme}>
+                    <Stack direction="row" spacing={1}>
+                        <Chip label="Javascript ES6+" color="primary" variant="outlined" />
+                        <Chip label="React.js" color="primary" variant="outlined" />
+                        <Chip label="PHP 8+" color="primary" variant="outlined" />
+                        <Chip label="Laravel" color="primary" variant="outlined" />
+                        <Chip label="HTML & CSS" color="primary" variant="outlined" />
+                        <Chip label="SCSS/SASS" color="primary" variant="outlined" />
+                        <Chip label="Amazon Web Services" color="primary" variant="outlined" />
+                    </Stack>
+                </ThemeProvider>
             </div>
         </section>
     );
