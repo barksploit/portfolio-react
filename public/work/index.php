03d89e0ca1544d0a3dda43a218655945cc880f6c
Thomas Griffin <tom.a.griffin@outlook.com>
2022-07-16 05:47:10 +0100

    Create PHP script that fetches GitHub repositories using API key

--- public/work/index.php
+++ public/work/index.php
@@ -1,4 +1,26 @@
 <?php
 
+$page = 1;
 
-?>
+if (isset($_GET["offset"]))
+    $page = $_GET["offset"];
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://api.github.com/users/barksploit/repos?sort=updated&per_page=5&page=" . $page);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$headers = [
+    'Accept: application/vnd.github+json',
+    'Authorization: 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',
+    'User-Agent: barksploit'
+];
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$output = curl_exec($ch);
+
+var_dump($output);
+
+curl_close($ch);
