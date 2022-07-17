bb9525358c6e0b6b8e2d35e2893d9b4041bdbf80
Thomas Griffin <tom.a.griffin@outlook.com>
2022-07-17 02:33:26 +0100

    Added view repository button to works component section

--- src/components/work.js
+++ src/components/work.js
@@ -1,8 +1,23 @@
 import '../styles/work.scss';
 import 'react-typist/dist/Typist.css';
 import axios from 'axios';
+import Button from '@mui/material/Button';
 import FadeInSection from './fadeinsection';
 import { useEffect, useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            // Purple and green play nicely together.
+            main: '#000',
+        },
+        secondary: {
+            // This is green.A700 as hex.
+            main: '#11cb5f',
+        },
+    },
+});
 
 const outputWorks = works => {
     let arr = [];
@@ -10,17 +25,18 @@ const outputWorks = works => {
         const work = works[iterator];
         arr.push(
             <FadeInSection key={iterator} delay={`${iterator}00ms`}>
-                <a href={work.html_url} target="_blank" rel="noreferrer">
-                    <div className="github-repository">
-                        <h2 className='work-title'>{work.full_name}</h2>
-                        <p className='work-description'>{work.description}</p>
-                        {work.topics.map((topic, i) => {
-                            return (
-                                <span key={i} className="work-topic">{topic}</span>
-                            )
-                        })}
-                    </div>
-                </a>
+                <div className="github-repository">
+                    <h2 className='work-title'>{work.full_name}</h2>
+                    <p className='work-description'>{work.description}</p>
+                    {work.topics.map((topic, i) => {
+                        return (
+                            <a key={i} href={`https://github.com/topics/${topic}`} target="_blank" rel="noreferrer"><span className="work-topic">{topic}</span></a>
+                        )
+                    })}
+                    <ThemeProvider theme={theme}>
+                        <Button className="view-repository" onClick={() => window.location = `${work.html_url}`} variant="outlined">view repository</Button>
+                    </ThemeProvider>
+                </div>
             </FadeInSection>
         )
     }
