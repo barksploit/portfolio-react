3d2da701c26d6f78860526f503a7738c94bf38fd
Thomas Griffin <tom@grfn.sh>
2022-07-18 21:44:28 +0100

    Fix section padding and margin behaviour and update header navigation active effect

--- src/components/header-navigation.js
+++ src/components/header-navigation.js
@@ -9,11 +9,10 @@ export default function HeaderNav() {
 
             <ul>
 
-                <li><Link activeClass="active" to="intro-wrapper" spy={true} smooth={true} duration={500}>.intro()</Link></li>
-                <li><Link activeClass="active" to="about" spy={true} smooth={true} duration={500}>.about()</Link></li>
-                <li><Link activeClass="active" to="experience" spy={true} smooth={true} duration={500}>.experience()</Link></li>
-                <li><Link activeClass="active" to="work" spy={true} smooth={true} duration={500}>.work()</Link></li>
-                <li><Link activeClass="active" to="credits" spy={true} smooth={true} duration={500}>.credits()</Link></li>
+                <li><Link isDynamic={true} activeClass="active" to="intro-wrapper" spy={true} smooth={true} duration={500}>.intro()</Link></li>
+                <li><Link isDynamic={true} activeClass="active" to="about" spy={true} smooth={true} duration={500}>.about()</Link></li>
+                <li><Link offset={25} isDynamic={true} activeClass="active" to="work" spy={true} smooth={true} duration={500}>.work()</Link></li>
+                <li><Link isDynamic={true} activeClass="active" to="credits" spy={true} smooth={true} duration={500}>.credits()</Link></li>
 
 
             </ul>
