9d1469be52b0f9e586492b7769cc5d4af11b5b0c
Thomas Griffin <tom@grfn.sh>
2022-07-20 00:58:09 +0100

    Fix for header navigation active effect sometimes not appearing for the credits component on clicking

--- src/components/header-navigation.js
+++ src/components/header-navigation.js
@@ -12,7 +12,7 @@ export default function HeaderNav() {
                 <li><Link isDynamic={true} activeClass="active" to="intro-wrapper" spy={true} smooth={true} duration={500}>.intro()</Link></li>
                 <li><Link isDynamic={true} activeClass="active" to="about" spy={true} smooth={true} duration={500}>.about()</Link></li>
                 <li><Link offset={25} isDynamic={true} activeClass="active" to="work" spy={true} smooth={true} duration={500}>.work()</Link></li>
-                <li><Link isDynamic={true} activeClass="active" to="credits" spy={true} smooth={true} duration={500}>.credits()</Link></li>
+                <li><Link offset={25} isDynamic={true} activeClass="active" to="credits" spy={true} smooth={true} duration={500}>.credits()</Link></li>
 
 
             </ul>
