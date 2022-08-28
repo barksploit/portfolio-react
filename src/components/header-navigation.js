661659aacb87a53dde3ebf7fa8e84ecda029fa61
Thomas Griffin <tom.a.griffin@outlook.com>
2022-08-28 13:04:02 +0100

    Added fade in transition to header and tweaked intro component

--- src/components/header-navigation.js
+++ src/components/header-navigation.js
@@ -1,7 +1,27 @@
 import '../styles/header-navigation.scss';
+import FadeInSection from './fadeinsection';
 import { Link } from 'react-scroll';
 
 export default function HeaderNav() {
+
+    const links = [
+        {
+            text: ".intro()",
+            to: "intro-wrapper"
+        },
+        {
+            text: ".about()",
+            to: "about"
+        },
+        {
+            text: ".work()",
+            to: "work"
+        },
+        {
+            text: ".credits()",
+            to: "credits"
+        }
+    ]
 
     return (
 
@@ -9,11 +29,16 @@ export default function HeaderNav() {
 
             <ul>
 
-                <li><Link isDynamic={true} activeClass="active" to="intro-wrapper" spy={true} smooth={true} duration={500}>.intro()</Link></li>
-                <li><Link isDynamic={true} activeClass="active" to="about" spy={true} smooth={true} duration={500}>.about()</Link></li>
-                <li><Link offset={25} isDynamic={true} activeClass="active" to="work" spy={true} smooth={true} duration={500}>.work()</Link></li>
-                <li><Link offset={25} isDynamic={true} activeClass="active" to="credits" spy={true} smooth={true} duration={500}>.credits()</Link></li>
 
+                {
+                    links.map((link, i) => {
+                        return (
+                            <FadeInSection key={link.text} delay={`${i}00ms`}>
+                                <li><Link isDynamic={true} activeClass="active" to={link.to} spy={true} smooth={true} duration={500}>{link.text}</Link></li>
+                            </FadeInSection>
+                        )
+                    })
+                }
 
             </ul>
 
